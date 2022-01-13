139d01d4788f87b31b6e1acdccb5ac5e9b6a878a
Maximiliano Ituarte <36338503+mituartel@users.noreply.github.com>
2022-01-12 23:04:14 -0300

    Commit
    primer commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Maximiliano Ituarte on 11/01/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // el que envia el mensaje
+    let body: String // el cuerpo del mensaje
+    
+}
